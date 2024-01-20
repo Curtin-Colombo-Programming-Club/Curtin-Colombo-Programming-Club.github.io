93afef42f074f93e9742dfda0c3afba376f9509b
Todo Lodo <me@todolodo.xyz>
2024-01-21 00:50:43 +0530

    Add GitHub Actions workflow and configure Express for GitHub Pages

--- src/server.js
+++ src/server.js
@@ -2,23 +2,18 @@ const http = require('http');
 const express = require('express');
 const path = require("path");
 
-
 function sendTemplate(res, file) {
     res.sendFile(`${path.join(process.cwd(), "./templates")}/${file}`);
 }
 
 
 const app = express();
+app.use(express.static('static'));
 const httpServer = http.createServer(app);
 
 //
 app.get("/", (req, res) => {
     sendTemplate(res, "index.html");
-})
-
-app.get("/static/:subDir/:file", async (req, res) => {
-    const { subDir, file } = req.params;
-    res.sendFile(path.join(process.cwd(), `./static/${subDir}/${file}`));
 })
 
 
